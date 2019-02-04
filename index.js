993bf3f6392bf2fde1209762eb12a0b9b861dd91
Gonçalo Andrade <goncaloandradedovale@gmail.com>
2019-02-04 10:58:04 +0000

    hash navigation

--- index.js
+++ index.js
@@ -6,18 +6,24 @@ import ntc from "./ntc.js";
 colors.forEach(x => {
     let elem = document.createElement("a");
     elem.style = `background-color:#${x}`;
-    elem.href = `?q=${x}`;
+    elem.href = `#q=${x}`;
     elem.classList = 'sample';
     document.querySelector('.colours-container').appendChild(elem);
 });
 
-let q = new URLSearchParams(window.location.search);
-q = decodeURI(q.get('q'));
-
-if (q && q.length && q !== 'null') {
-    ntc.init();
-    const cat = ntc.name(q);
-    document.querySelector('.colour').setAttribute('style', `border-color:${cat[0]} ${cat[0]} #${q} #${q}`);
-    document.querySelector('.hex').innerHTML = cat[0];
-    document.querySelector('.category').innerHTML = cat[1];
+const fn = {
+    getColour: () => {
+        const hash = window.location.hash.substr(3);
+        if (hash && hash.length && hash !== 'null') {
+            ntc.init();
+            const cat = ntc.name(hash);
+            document.querySelector('.colour').setAttribute('style', `border-color:${cat[0]} ${cat[0]} #${hash} #${hash}`);
+            document.querySelector('.hex').innerHTML = cat[0];
+            document.querySelector('.category').innerHTML = cat[1];
+        }
+    }
 }
+
+
+window.onhashchange = fn.getColour;
+fn.getColour();
